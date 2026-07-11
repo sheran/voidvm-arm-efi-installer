87e5c4649c91c22dc3483a07c4171fbb514ef71c
Sheran Gunasekera <sheran@zensay.com>
2026-07-11 21:04:00 +0800

    removing xbps neovim because I'll add it from my dotfiles later

--- stage2.sh
+++ stage2.sh
@@ -4,9 +4,14 @@ set -e
 
 : "${USER_PASSWORD:=voidlinux}"
 
-xbps-install -Sy xorg xfce4 neovim curl unzip spice-vdagent mesa-dri dejavu-fonts-ttf openssl
+xbps-install -Sy xorg xfce4 curl unzip spice-vdagent mesa-dri dejavu-fonts-ttf openssl \
+    ghostty git zsh wget python3 flatpak xdg-desktop-portal-gtk tailscale
 ln -sf /etc/sv/dbus /var/service/
 ln -sf /etc/sv/spice-vdagentd /var/service/
+ln -sf /etc/sv/tailscaled /var/service/
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y --or-update flathub com.brave.Browser
 id voiduser >/dev/null 2>&1 || useradd -mG wheel voiduser
 echo "exec startxfce4" > /home/voiduser/.xinitrc
 sed -i 's/#\s*\(%wheel ALL=(ALL:ALL) NOPASSWD: ALL\)/\1/' /etc/sudoers
